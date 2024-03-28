7ecf6e0dc750f6a47e7cd5c92392865701e2ba39
yasin-arft <yasinarftsocial@gmail.com>
2024-03-28 12:41:38 +0600

    added go back button on error page

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
-console.log(error);
+  const navigate = useNavigate();
+  
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-4">
       <h3 className="text-2xl font-semibold">Oops!</h3>
       <h2 className="text-4xl font-bold">{error.status}</h2>
       <p>{error.message || error.statusText}</p>
+      <button onClick={() => navigate(-1)} className="btn bg-dark text-white hover:text-dark">Go Back</button>
     </div>
   );
 };
